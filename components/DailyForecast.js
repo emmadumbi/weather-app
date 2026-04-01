9a7d6671cbc5010d7bd108cd675c65cfa6134a99
emmadumbi <dumbiemmanuel29@gmail.com>
2026-04-01 16:50:38 +0100

    complete the initial stage of ui

--- components/DailyForecast.js
+++ components/DailyForecast.js
@@ -2,65 +2,81 @@ import DrizzleIcon from "../src/assets/images/icon-drizzle.webp";
 
 export default function dailyforecast() {
   return /*html*/ `
-        <section>
-            <h3>Daily forecast</h3>
-            <div>
-                <div>
-                    <p>Tue</p>
+        <section class="mt-7">
+            <h3 class="text-[20px]">Daily forecast</h3>
+            <div class="three_col-grid">
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
                 </div>
-                <div>
-                    <p>Tue</p>
+                
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
                 </div>
-                <div>
-                    <p>Tue</p>
+
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
                 </div>
-                <div>
-                    <p>Tue</p>
+
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
                 </div>
-                <div>
-                    <p>Tue</p>
+
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
                 </div>
-                <div>
-                    <p>Tue</p>
+
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
                     <span>
-                       <img src="${DrizzleIcon}" alt=""> 
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
                     </span>
-                    <div>
+                    <div class="flex justify-between w-full">
+                        <p>68&deg;</p>
+                        <p>57&deg;</p>
+                    </div>
+                </div>
+
+                <div class="bg-myneutral-800 flex flex-col items-center rounded-small px-2 pt-3 space-y-3 pb-4">
+                    <p class="text-[18px]">Tue</p>
+                    <span>
+                       <img src="${DrizzleIcon}" alt="" width="60" height="60"> 
+                    </span>
+                    <div class="flex justify-between w-full">
                         <p>68&deg;</p>
                         <p>57&deg;</p>
                     </div>
--- components/HourForecast.js
+++ components/HourForecast.js
@@ -2,55 +2,72 @@ import SnowIcon from "../src/assets/images/icon-snow.webp";
 
 export default function hourforecast() {
   return /*html*/ `
-        <section>
-            <div>
-                <h4></h4>
-                <div>Tuesday</div>
+        <section class="mt-9 bg-myneutral-800 py-5 px-4  rounded-small space-y-4">
+            <div class="flex items-center justify-between mb-5">
+                <h4 class="text-[20px] font-medium">Hourly forecast</h4>
+                <div class="bg-myneutral-600 py-1.5 px-5 rounded-small"><span>Tuesday</span> <span>&UpArrowDownArrow;</span></div>
             </div>
-            <div>
-                <div>
+
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
             
-            <div>
-                <div>
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
 
-            <div>
-                <div>
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
 
-            <div>
-                <div>
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
 
-            <div>
-                <div>
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
 
-            <div>
-                <div>
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
                     <img src="${SnowIcon}" alt="" width="40" height="40">
-                    <p>3PM</p>
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
+                </div>
+                <p>68&deg;</p>
+            </div>
+
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
+                    <img src="${SnowIcon}" alt="" width="40" height="40">
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
+                </div>
+                <p>68&deg;</p>
+            </div>
+
+            <div class="flex items-center justify-between  bg-myneutral-600 p-2.5 rounded-small">
+                <div class="flex items-center gap-x-2">
+                    <img src="${SnowIcon}" alt="" width="40" height="40">
+                    <p class="text-[20px] font-medium">3 <span>PM</span></p>
                 </div>
                 <p>68&deg;</p>
             </div>
